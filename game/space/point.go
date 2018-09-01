b09fb596c91fea77cb8e2337dc71491107f02dbd
municorn <municorn@musicaloft.com>
2018-08-31 18:02:39 -0600

    added Point and Velocity structs

---
+++ game/space/point.go
@@ -0,0 +1,6 @@
+package space
+
+// Point holds an x and y coordinate. it is two-dimensional.
+type Point struct {
+	X, Y float32
+}
---
+++ game/space/velocity.go
@@ -0,0 +1,6 @@
+package space
+
+// Velocity holds a dx and dy vector.
+type Velocity struct {
+	Dx, Dy float32
+}
