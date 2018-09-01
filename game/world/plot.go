cff97ff6da593d6d80838aa50ba7483d715c1fc4
municorn <municorn@musicaloft.com>
2018-08-31 18:07:20 -0600

    added some comments to the Plot struct

--- game/world/plot.go
+++ game/world/plot.go
@@ -4,10 +4,14 @@ import b "chicky-chicky-go/game/blocks"
 
 // Plot contains a three-dimensional array of blocks
 type Plot struct {
+	// blocks is two blocks deep. there can be one
+	// block in the back and one in the front. blocks not
+	// supported by a block behind it will fall.
 	blocks [128][64][2]b.Block // height, width, depth
 }
 
 // IsOnScreen returns true if Plot p is visible on the screen
+// TODO
 func (p Plot) IsOnScreen() bool {
 	return false;
 }
